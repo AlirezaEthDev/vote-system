b908f256912d35c4e3970b074cdc7c1ac87e16c5
AlirezaEthDev <alirezaethdev@gmail.com>
2024-06-09 20:15:28 +0430

    Code style is clean

--- 1_Migration.js
+++ 1_Migration.js
@@ -1,50 +1,9 @@
 
-const Web3 = require("web3");
-const web3=new Web3("http://localhost:7545");
-
-const signatureObject = web3.eth.accounts.sign( "Alireza Kiakojouri" , "0x4aef83682bca621d05a70ea0a742506fe29d9672427f8ddc821ecda2228a59d1" );
-
-console.info( signatureObject );
-
-const voteSystem = artifacts.require("./voteSystem.sol");
-const voteLibrary = artifacts.require("./voteLibrary.sol");
+const voteSystem = artifacts.require("./VoteSystem.sol");
+const voteLibrary = artifacts.require("./VoteLibrary.sol");
 
 module.exports = function(deployer) {
-
-  const candidate1 = {
-
-    firstname : Buffer.from( "Alireza" , "utf-8" ),
-    lastname : Buffer.from( "Kiakojouri" , "utf-8" ),
-    id : 3657700005,
-    voteID : 1,
-    voteCount : 0
-
-  }
-
-  const candidate2 = {
-
-    firstname : Buffer.from( "Babak" , "utf-8" ),
-    lastname : Buffer.from( "Arjmand" , "utf-8" ),
-    id : 856325654,
-    voteID : 2,
-    voteCount : 0
-
-  }
-
-  const candidate3 = {
-
-    firstname : Buffer.from( "Pedram" , "utf-8" ),
-    lastname : Buffer.from( "Nazeri" , "utf-8" ),
-    id : 8754553465,
-    voteID : 3,
-    voteCount : 0
-    
-  }
-
-  const listOfCandidates = [ candidate1 , candidate2 , candidate3 ];
-
-  deployer.deploy( voteLibrary );
-  deployer.link( voteLibrary , voteSystem );
-  deployer.deploy( voteSystem );
-
+  deployer.deploy(voteLibrary);
+  deployer.link(voteLibrary, voteSystem);
+  deployer.deploy(voteSystem);
 }
--- votingSystem.sol
+++ votingSystem.sol
@@ -4,217 +4,156 @@
 
 pragma solidity ^0.8.0;
 
-library voteLibrary{
+library VoteLibrary{
+    // Constants
+    address constant public TRUSTED_ACCOUNT = 0xF3ca7784b075Bbc683Aaeb18C11E47E5ee3f3350;
+    bytes4 constant public MAGIC_VALUE = 0x1626ba7e;
 
-    address constant public trustedAccount = 0xF3ca7784b075Bbc683Aaeb18C11E47E5ee3f3350;
-    bytes4 constant public magicValue = 0x1626ba7e;
-
-    struct voteCounting{
-
+    // Structs
+    struct VoteCounting{
         uint8 voteID;
         uint voteCount;
-
     }
 
-    function voteCounter( voteCounting[] calldata countResult ) external view returns( voteCounting[] memory ) {
-
-        voteCounting[] memory sortedCountResult = new voteCounting[]( countResult.length );//Calldata array is read-only. So we need to declare another array!
-
-        for( uint i = 0 ; i < countResult.length ; i++ ){
-
-            sortedCountResult[ i ] = countResult[ i ];
-
+    // Functions
+    function voteCounter(VoteCounting[] calldata countResult) external view returns(VoteCounting[] memory) {
+        // Sort the vote counts
+        VoteCounting[] memory sortedCountResult = new VoteCounting[](countResult.length);// Calldata array is read-only. So we need to use another array instead countResult!
+        for (uint i = 0 ; i < countResult.length ; i++) {
+            sortedCountResult[i] = countResult[i];
         }
-
-        voteCounting memory tempCounting;
-
-        for( uint i = 0 ; i < sortedCountResult.length ; i++ ){
-
-            for( uint j = i + 1 ; j < sortedCountResult.length ; j++){
-
-                if( sortedCountResult[ i ].voteCount < sortedCountResult[ j ].voteCount ){
-
-                    tempCounting = sortedCountResult[ i ];
-                    sortedCountResult[ i ] = sortedCountResult[ j ];
-                    sortedCountResult[ j ] = voteCounting( tempCounting.voteID , tempCounting.voteCount );
-
+        VoteCounting memory tempCounting;
+        for (uint i = 0 ; i < sortedCountResult.length ; i++) {
+            for (uint j = i + 1 ; j < sortedCountResult.length ; j++) {
+                if (sortedCountResult[i].voteCount < sortedCountResult[j].voteCount) {
+                    tempCounting = sortedCountResult[i];
+                    sortedCountResult[i] = sortedCountResult[j];
+                    sortedCountResult[j] = VoteCounting(tempCounting.voteID, tempCounting.voteCount);
                 }
-
             }
-
         }
-
         return sortedCountResult;
-
     }
-
-    function isValidSignature( bytes32 _hash , bytes calldata _signature ) external view returns ( bytes4 ){
-
-        address signer = recoverSignature( _hash , _signature );
-
-        if( signer == trustedAccount ){
-
+    
+    function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bytes4) {
+        address signer = recoverSignature(hash, signature);
+        if (signer == TRUSTED_ACCOUNT) {
             return 0x1626ba7e;
-
         }else{
-
             return 0xffffffff;
-
         }
-
     }
 
-    function recoverSignature( bytes32 _hash , bytes memory _signature ) public pure returns( address ){
+    function recoverSignature(bytes32 hash, bytes memory signature) public pure returns(address) {
+        // Is signature an Ethereum signature
+        require(signature.length == 65 , "01");//01: Signature length is not valid!
 
-        require( _signature.length == 65 , "01" );
-        //01: Signature length is not valid!
-
-        uint8 v = uint8( _signature[ 64 ] );
+        // Validate the provided Ethereum signature (signature)
+        uint8 v = uint8( signature[ 64 ] );
         bytes32 r;
         bytes32 s;
-
         assembly {
-
-            r := mload( add( _signature , 0x20 ) )
-            s := mload( add( _signature , 0x40 ) )
-
+            r := mload( add( signature , 0x20 ) )
+            s := mload( add( signature , 0x40 ) )
         }
+        require(uint256( s ) <= 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0,
+                    "02");// 02: The 's' value of the signature is not valid!
+        require(v == 27 || v == 28, "03");// 03: The 'v' value of the signature is not valid!
 
-        require( uint256( s ) <= 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0 ,
-                    " 02 " );
-        //02: The 's' value of the signature is not valid!
-        require( v == 27 || v == 28 , " 03 " );
-        //03: The 'v' value of the signature is not valid!
+        // Recover the signer's address from the signature
+        address signer = ecrecover(hash, v, r, s);
 
-        address signer = ecrecover( _hash , v , r , s );
-
-        require( signer != address( 0x0 ) , " 04 " );
-        //04: The signer address is not valid!
-
+        // Validate the structure of the recovered address
+        require( signer != address( 0x0 ), "04");// 04: The signer address is not valid!
         return signer;
-
     }
-
 }
 
-contract voteSystem{
-
-    address constant public trustedAccount = 0xF3ca7784b075Bbc683Aaeb18C11E47E5ee3f3350; //This address can change for each project using this contract!
+contract VoteSystem{
+    // Constants
+    address constant public TRUSTED_ACCOUNT = 0xF3ca7784b075Bbc683Aaeb18C11E47E5ee3f3350; // This address can change for each project using this contract!
     uint public startOfVotePeriod;
     uint public endOfVotePeriod;
     uint public candidateCounter=0;
 
-    struct candidate{
-
+    // Structs
+    struct Candidate{
         bytes firstname;
         bytes lastname;
         uint40 id;
         uint8 voteID;
         uint voteCount;
-
     }
 
-    event newCandidateAdded( bytes fullname , uint40 id , uint8 voteID );
-    event votePeriodUpdate( uint startAt , uint endAt );
-    event newVoteTo( bytes votedCandidate , uint candidatesVoteID , address voter );
+    // Events
+    event NewCandidateAdded(bytes fullname, uint40 id, uint8 voteID);
+    event VotePeriodUpdate(uint startAt, uint endAt);
+    event NewVoteTo(bytes votedCandidate, uint candidatesVoteID, address voter);
 
-    mapping( uint8 => candidate ) private candidateList;
-    mapping( address => bool ) private revoteCheck;
+    // Mappings
+    mapping(uint8 => Candidate) private candidateList;
+    mapping(address => bool) private revoteCheck;
 
-
-    modifier onlyTrustedAccount( address accountAddress ) {
-
-        require( trustedAccount == accountAddress , "05" );
-        //05: Just the trusted account has access to do this task!
+    // Modifiers
+    modifier onlyTrustedAccount(address accountAddress) {
+        require( TRUSTED_ACCOUNT == accountAddress, "05" );// 05: Just the trusted account has access to do this task!
         _;
-
     }
 
-    function addCandidateList( candidate[] calldata listOfCandidates ) external onlyTrustedAccount(msg.sender) {
-
+    // Functions
+    function addCandidateList(Candidate[] calldata listOfCandidates) external onlyTrustedAccount(msg.sender) {
         candidateCounter = 0;
-
-        for ( uint i = 0 ; i < listOfCandidates.length ; i++ ){
-
-            candidateList[listOfCandidates[i].voteID] = candidate( listOfCandidates[i].firstname , listOfCandidates[i].lastname , listOfCandidates[i].id , listOfCandidates[i].voteID , listOfCandidates[i].voteCount );
+        for (uint i = 0 ; i < listOfCandidates.length ; i++) {
+            candidateList[listOfCandidates[i].voteID] = Candidate(listOfCandidates[i].firstname, listOfCandidates[i].lastname, listOfCandidates[i].id, listOfCandidates[i].voteID, listOfCandidates[i].voteCount);
             candidateCounter++;
-            emit newCandidateAdded( abi.encodePacked( listOfCandidates[i].firstname , listOfCandidates[i].lastname ) , listOfCandidates[i].id , listOfCandidates[i].voteID );
-
+            emit NewCandidateAdded(abi.encodePacked(listOfCandidates[i].firstname, listOfCandidates[i].lastname), listOfCandidates[i].id, listOfCandidates[i].voteID);
         }
-
     }
 
-    function viewList( uint8 candidateVoteID) external view returns( candidate memory ){
-
-        return ( candidateList[ candidateVoteID ] );
-
+    function viewList(uint8 candidateVoteID) external view returns(Candidate memory) {
+        return (candidateList[candidateVoteID]);
     }
 
-    function setVotePeriod( uint startTime , uint endTime ) external onlyTrustedAccount( msg.sender ) {
-
-        require( startOfVotePeriod == 0 || block.timestamp < startOfVotePeriod , "06" );
-        //06: Reset the vote period is not allowed!
+    function setVotePeriod(uint startTime, uint endTime) external onlyTrustedAccount(msg.sender) {
+        // Ensures that this function is executed before the voting period starts
+        require(startOfVotePeriod == 0 || block.timestamp < startOfVotePeriod, "06");// 06: Reset the vote period is not allowed!
         
+        // Sets the start and end times for voting
         startOfVotePeriod = startTime;
         endOfVotePeriod = endTime;
-
-        emit votePeriodUpdate( startOfVotePeriod , endOfVotePeriod );
-
+        emit VotePeriodUpdate(startOfVotePeriod, endOfVotePeriod);
     }
 
-    function voteTo( uint8 candidateVoteID , bytes32 voteHash , bytes calldata trustedAccountSignature ) external {
-
-        require( startOfVotePeriod!= 0 && block.timestamp >= startOfVotePeriod && block.timestamp <= endOfVotePeriod , " 07 " );
-        //07: Out of vote period!
+    function voteTo(uint8 candidateVoteID, bytes32 voteHash, bytes calldata trustedAccountSignature) external {
+        // Ensures that this function only executed during the voting period
+        require(startOfVotePeriod!= 0 && block.timestamp >= startOfVotePeriod && block.timestamp <= endOfVotePeriod, "07");// 07: Out of vote period!    
         
-        //Validating revote
-        require( !revoteCheck[ msg.sender ] , " 08 " );
-        //08: Revoting is not allowed!
-
-        //Validating signature
-        bytes4 validationResult = voteLibrary.isValidSignature( voteHash , trustedAccountSignature );
-        require( validationResult == 0x1626ba7e , " 09 " );
-        //09: You are not an eligible voter!
-
-        //Counting the vote
+        //Validate revote
+        require(!revoteCheck[ msg.sender ], "08");// 08: Revoting is not allowed!
+        
+        //Validate signature
+        bytes4 validationResult = VoteLibrary.isValidSignature(voteHash, trustedAccountSignature);
+        require(validationResult == 0x1626ba7e, "09");// 09: You are not an eligible voter!
+        
+        //Count the vote
         uint codeSize;
         address msgSender = msg.sender;
-
         assembly {
-
             codeSize := extcodesize( msgSender )
-
         }
-
-        if( codeSize == 0){
-
-            candidateList[ candidateVoteID ].voteCount++;
-            revoteCheck[ msg.sender ] = true;
-
-            emit newVoteTo( abi.encodePacked( candidateList[ candidateVoteID ].firstname , candidateList[ candidateVoteID ].lastname ) ,
-                            candidateList[ candidateVoteID ].voteID , msg.sender );
-
-        }else{
-
-            revert( " 10 " );
-            //10: Smart contract is not allowed to vote!
-
-        }
-
+        require(codeSize == 0, "10");// 10: Smart contract is not allowed to vote!
+        candidateList[candidateVoteID].voteCount++;
+        revoteCheck[msg.sender] = true;
+        emit NewVoteTo(abi.encodePacked(candidateList[candidateVoteID].firstname, candidateList[candidateVoteID].lastname),candidateList[candidateVoteID].voteID, msg.sender);
     }
 
-    function countVote() external view returns( voteLibrary.voteCounting[] memory ){
-
-        voteLibrary.voteCounting[] memory voteCountingArray;
-        voteCountingArray = new voteLibrary.voteCounting[]( candidateCounter );
-
-        for( uint8 i = 0 ; i < candidateCounter ; i++ ){
-
-            voteCountingArray[ i ] = voteLibrary.voteCounting( candidateList[ i + 1 ].voteID , candidateList[ i + 1 ].voteCount );
-
+    function countVote() external view returns(VoteLibrary.VoteCounting[] memory) {
+        // Instantiation of an array with type of the library's struct
+        VoteLibrary.VoteCounting[] memory voteCountingArray;
+        voteCountingArray = new VoteLibrary.VoteCounting[](candidateCounter);
+        for (uint8 i = 0 ; i < candidateCounter ; i++) {
+            voteCountingArray[i] = VoteLibrary.VoteCounting(candidateList[i + 1].voteID, candidateList[i + 1].voteCount);
         }
-
-        return voteLibrary.voteCounter( voteCountingArray );
+        return VoteLibrary.voteCounter(voteCountingArray);
     }
-
 }
